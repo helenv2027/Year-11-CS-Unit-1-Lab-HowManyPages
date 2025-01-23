38cf6ec004daad14cd0b01078adcb35af78275ec
helen <helenvong2009@gmail.com>
2025-01-23 10:03:57 +0800

    first commit

--- src/HowManyPages.java
+++ src/HowManyPages.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class HowManyPages {
   
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter your age:");
+    int age= scanner.nextInt();
+    System.out.println(age+" year olds should read at least "+(100-age)+" pages before giving up on a book.");
     
   }
   
